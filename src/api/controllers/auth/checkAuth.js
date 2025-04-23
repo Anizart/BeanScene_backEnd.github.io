f4d18dfbf605582303abb1c2cf554b223c39dfb1
Anizart <gitanizart@gmail.com>
2025-04-23 23:52:52 +0500

    checkAuth fix

--- src/api/controllers/auth/checkAuth.js
+++ src/api/controllers/auth/checkAuth.js
@@ -1,11 +1,11 @@
 //+ Ссылка офиса (прверка):
 //+ Эндпоинт для проверки авторизации:
 export const checkAuth = (req, res) => {
-    const userId = req.cookies.userId;
+	const userId = req.cookies?.userId;
 
-    if (userId) {
-        return res.json({isAuthenticated: true});
-    } else {
-        return res.json({isAuthenticated: false});
-    }
+	if (!userId) {
+		return res.status(401).json({ message: "Not authenticated" });
+	}
+
+	return res.status(200).json({ isAuthenticated: true, userId });
 };
